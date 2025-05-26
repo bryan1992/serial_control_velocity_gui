2556db82e53fd343a00ea6849fe29f3b6dadbfea
bryan1992 <bryanstratovarius@hotmail.com>
2025-05-25 18:24:48 -0600

    Se agregó cambio de sentido del motor y detención.

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -3,12 +3,14 @@ const int stepX = 2;
 const int dirX = 5;
 const int enPin = 8;
 
-unsigned long tiempo_anterior = micros();
-unsigned long tiempo_actual = 0;
-unsigned long tiempo_toggle = 0;
-unsigned long tiempo_acumulado = 0;
+unsigned long tiempoAnterior = micros();
+unsigned long tiempoActual = 0;
+unsigned long tiempoToggle = 0;
 unsigned long intervalo = 800;
+unsigned long ultimoPrint;
+int valor = 800;
 bool pulso = true;
+String serialBuffer;
 
 void setup() {
   pinMode(stepX, OUTPUT);
@@ -19,16 +21,62 @@ void setup() {
   digitalWrite(enPin, LOW);   // Habilita el driver (LOW = habilitado)
 
   Serial.begin(9600);
+  delay(200);
+  Serial.println("Iniciando...");
+  Serial.print("La velocidad actual es: ");
+  Serial.println(intervalo);
+  ultimoPrint = millis();
 }
 
 void loop() {
-  tiempo_actual = micros();
-  
-  if (tiempo_actual - tiempo_anterior >= intervalo)
+
+  // Impresión periódica de intervalo.
+  if (millis() - ultimoPrint >= 2000)
     {
-      tiempo_anterior = tiempo_actual;
-      tiempo_toggle = tiempo_actual;
-      pulso = !pulso;
-      digitalWrite(stepX, pulso);
+      Serial.print("La velocidad actual es: ");
+      Serial.println(valor);
+      ultimoPrint = millis();
     }
+  
+  // Control de motor.
+  tiempoActual = micros();
+  if (tiempoActual - tiempoAnterior >= intervalo)
+    {
+      tiempoAnterior = tiempoActual;
+      tiempoToggle = tiempoActual;
+      pulso = !pulso;
+
+      // Pulso de dirección.
+      if (valor >= 0)
+        {
+          digitalWrite(dirX, HIGH);
+        }
+      else
+        {
+          digitalWrite(dirX, LOW);
+        }
+
+      // Pulso de movimiento.
+      if (valor != 0)
+        {
+          digitalWrite(stepX, pulso);
+        }      
+    }
+
+  // Comunicación serial.
+  while (Serial.available() > 0)
+  {
+    char c = Serial.read();
+    if (c == '\n')
+      {
+        valor = serialBuffer.toInt();
+        intervalo = abs(valor);
+        Serial.println(valor);
+        serialBuffer = "";
+      }
+    else if (isDigit(c) || (c == '-'))
+      {
+        serialBuffer += c; // Concatenar caracteres en buffer, antes de recibir \n.
+      }
+  } 
 }
